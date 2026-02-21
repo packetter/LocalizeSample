14cd677c72aa26e1d1639b6031cb561911597047
packetter <packetter@gmail.com>
2026-02-22 07:47:41 +0900

    Initial commit

---
+++ LocalizeSample/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using WPFLocalizeExtension.Engine;
+
+namespace LocalizeSample
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            LocalizeDictionary.Instance.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "Culture")
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FormattedTitle)));
+            };
+        }
+
+        public string TitleKey => "Title";
+
+        public string[] Args => ["arg1"];
+
+        public string FormattedTitle =>
+            string.Format(
+                LocalizeDictionary.Instance.GetLocalizedObject(
+                    "LocalizeSample", "Resources", TitleKey,
+                    LocalizeDictionary.Instance.Culture) as string ?? "",
+                Args);
+    }
+}
